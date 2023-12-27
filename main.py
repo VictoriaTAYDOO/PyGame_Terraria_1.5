7d073910eaf482ef8dd8434fea567436e81476d6
anastasiakrahmalnikova <anastasia.krakhmalnikova@yandex.ru>
2023-12-28 00:48:09 +0300

    load level commit

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import os
-import random
 import sys
 
 import pygame
 
 pygame.init()
-size = width, height = 900, 600
+size = width, height = 1000, 600
 screen = pygame.display.set_mode(size)
 FPS = 50
 clock = pygame.time.Clock()
@@ -26,14 +25,98 @@ def terminate():
     sys.exit()
 
 
-def start_screen():
-    intro_text = "НАЧАТЬ"
+class Hero(pygame.sprite.Sprite):
+    def __init__(self):
+        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
+        # Это очень важно !!!
+        super().__init__(all_sprites)
+        self.hero_im = load_image("hero.png")
+        self.image = self.hero_im
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        self.rect.y = 0
 
+
+all_sprites = pygame.sprite.Group()
+
+
+def load_level_1():
+    level = pygame.transform.scale(load_image('level1.png'), size)
+    screen.blit(level, (0, 0))
+    player = Hero()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_DOWN:
+                    player.rect.y += 10
+                elif event.key == pygame.K_UP:
+                    player.rect.y -= 10
+                elif event.key == pygame.K_LEFT:
+                    player.rect.x -= 10
+                elif event.key == pygame.K_RIGHT:
+                    player.rect.x += 10
+        screen.blit(level, (0, 0))
+        all_sprites.draw(screen)
+        all_sprites.update()
+        clock.tick(10)
+        pygame.display.flip()
+
+
+def load_level_2():
+    level = pygame.transform.scale(load_image('level2.png'), size)
+    screen.blit(level, (0, 0))
+    player = Hero()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_DOWN:
+                    player.rect.y += 10
+                elif event.key == pygame.K_UP:
+                    player.rect.y -= 10
+                elif event.key == pygame.K_LEFT:
+                    player.rect.x -= 10
+                elif event.key == pygame.K_RIGHT:
+                    player.rect.x += 10
+        screen.blit(level, (0, 0))
+        all_sprites.draw(screen)
+        all_sprites.update()
+        clock.tick(10)
+        pygame.display.flip()
+
+
+def load_level_3():
+    level = pygame.transform.scale(load_image('level3.png'), size)
+    screen.blit(level, (0, 0))
+    player = Hero()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_DOWN:
+                    player.rect.y += 10
+                elif event.key == pygame.K_UP:
+                    player.rect.y -= 10
+                elif event.key == pygame.K_LEFT:
+                    player.rect.x -= 10
+                elif event.key == pygame.K_RIGHT:
+                    player.rect.x += 10
+        screen.blit(level, (0, 0))
+        all_sprites.draw(screen)
+        all_sprites.update()
+        clock.tick(10)
+        pygame.display.flip()
+
+
+def start_screen():
+    play = pygame.transform.scale(load_image('play.png'), (300, 100))
     fon = pygame.transform.scale(load_image('fon.jpg'), (size))
     screen.blit(fon, (0, 0))
-    font = pygame.font.Font(None, 60)
-    string_rendered = font.render(intro_text, 1, pygame.Color('black'))
-    screen.blit(string_rendered, (370, 350))
+    screen.blit(play, (350, 300))
 
     while True:
         for event in pygame.event.get():
@@ -41,99 +124,48 @@ def start_screen():
                 terminate()
             elif event.type == pygame.KEYDOWN or \
                     event.type == pygame.MOUSEBUTTONDOWN:
-                return  # начинаем игру
+                if 350 < pygame.mouse.get_pos()[0] < 650 and 300 < pygame.mouse.get_pos()[1] < 400:
+                    level_menu_screen()
         pygame.display.flip()
-        clock.tick(FPS)
+
+
+def level_menu_screen():
+    easy = pygame.transform.scale(load_image('easy.png'), (150, 150))
+    medium = pygame.transform.scale(load_image('medium.png'), (150, 150))
+    hard = pygame.transform.scale(load_image('hard.png'), (150, 150))
+    fon = pygame.transform.scale(load_image('fon.jpg'), (size))
+    screen.blit(fon, (0, 0))
+    screen.blit(easy, (200, 250))
+    screen.blit(medium, (425, 250))
+    screen.blit(hard, (650, 250))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                if 200 < pygame.mouse.get_pos()[0] < 350 and 250 < pygame.mouse.get_pos()[1] < 400:
+                    load_level_1()
+                elif 425 < pygame.mouse.get_pos()[0] < 575 and 250 < pygame.mouse.get_pos()[1] < 400:
+                    load_level_2()
+                elif 650 < pygame.mouse.get_pos()[0] < 800 and 250 < pygame.mouse.get_pos()[1] < 400:
+                    load_level_3()
+        pygame.display.flip()
 
 
 start_screen()
+level_menu_screen()
 
 
-def load_level(filename):
-    filename = "data/" + filename
-    # читаем уровень, убирая символы перевода строки
-    with open(filename, 'r') as mapFile:
-        level_map = [line.strip() for line in mapFile]
-
-    # и подсчитываем максимальную длину
-    max_width = max(map(len, level_map))
-
-    # дополняем каждую строку пустыми клетками ('.')
-    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
-
-
-tile_images = {
-    'wall': load_image('box.png'),
-    'empty': load_image('grass.png')
-}
-player_image = load_image('mar.png')
-
-tile_width = tile_height = 50
-
-
-class Tile(pygame.sprite.Sprite):
-    def __init__(self, tile_type, pos_x, pos_y):
-        super().__init__(tiles_group, all_sprites)
-        self.image = tile_images[tile_type]
-        self.rect = self.image.get_rect().move(
-            tile_width * pos_x, tile_height * pos_y)
-
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y):
-        super().__init__(player_group, all_sprites)
-        self.image = player_image
-        self.rect = self.image.get_rect().move(
-            tile_width * pos_x + 15, tile_height * pos_y + 5)
-
-
-# основной персонаж
-player = None
-
-# группы спрайтов
-all_sprites = pygame.sprite.Group()
-tiles_group = pygame.sprite.Group()
-player_group = pygame.sprite.Group()
-
-
-def generate_level(level):
-    new_player, x, y = None, None, None
-    for y in range(len(level)):
-        for x in range(len(level[y])):
-            if level[y][x] == '.':
-                Tile('empty', x, y)
-            elif level[y][x] == '#':
-                Tile('wall', x, y)
-            elif level[y][x] == '@':
-                Tile('empty', x, y)
-                new_player = Player(x, y)
-    # вернем игрока, а также размер поля в клетках
-    return new_player, x, y
-
-
-player, level_x, level_y = generate_level(load_level('1.txt'))
-
-level = load_level('1.txt')
 
 running = True
-clock = pygame.time.Clock()
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_DOWN and level[player.rect.y // 50 + 1][player.rect.x // 50] != '#':
-                player.rect.y += 50
-            elif event.key == pygame.K_UP and level[player.rect.y // 50 - 1][player.rect.x // 50] != '#':
-                player.rect.y -= 50
-            elif event.key == pygame.K_LEFT and level[player.rect.y // 50][player.rect.x // 50 - 1] != '#':
-                player.rect.x -= 50
-            elif event.key == pygame.K_RIGHT and level[player.rect.y // 50][player.rect.x // 50 + 1] != '#':
-                player.rect.x += 50
     screen.fill(pygame.Color("white"))
     all_sprites.draw(screen)
-    tiles_group.draw(screen)
-    player_group.draw(screen)
     all_sprites.update()
     clock.tick(10)
     pygame.display.flip()
