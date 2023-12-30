6c8423737e0df265d6b3510062144c5e941f9bcf
anastasiakrahmalnikova <anastasia.krakhmalnikova@yandex.ru>
2023-12-30 19:09:25 +0300

    jump commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pygame
 pygame.init()
 size = width, height = 1000, 600
 screen = pygame.display.set_mode(size)
-FPS = 50
+FPS = 100
 clock = pygame.time.Clock()
 
 
@@ -27,60 +27,111 @@ def terminate():
 
 class Hero(pygame.sprite.Sprite):
     def __init__(self):
-        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
-        # Это очень важно !!!
         super().__init__(all_sprites)
         self.hero_im = load_image("hero.png")
         self.image = self.hero_im
         self.rect = self.image.get_rect()
-        self.rect.x = 0
-        self.rect.y = 0
+        self.rect.x = 50
+        self.rect.y = 550
 
 
 all_sprites = pygame.sprite.Group()
 
 
 def load_level_1():
+    pygame.mixer.music.load('data/boss1.ogg')
+    pygame.mixer.music.play()
     level = pygame.transform.scale(load_image('level1.png'), size)
     screen.blit(level, (0, 0))
     player = Hero()
+    flLeft = flRight = flup = fldown = False
+    jump = False
+    jumpCount = 0
+    jumpMax = 15
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_DOWN:
-                    player.rect.y += 10
-                elif event.key == pygame.K_UP:
-                    player.rect.y -= 10
-                elif event.key == pygame.K_LEFT:
-                    player.rect.x -= 10
-                elif event.key == pygame.K_RIGHT:
-                    player.rect.x += 10
+                if event.key == pygame.K_s:
+                    fldown = True
+                elif not jump and event.key == pygame.K_SPACE:
+                    jump = True
+                    jumpCount = jumpMax
+                elif event.key == pygame.K_w:
+                    flup = True
+                elif event.key == pygame.K_a:
+                    flLeft = True
+                elif event.key == pygame.K_d:
+                    flRight = True
+            elif event.type == pygame.KEYUP:
+                if event.key in [pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s]:
+                    flLeft = flRight = flup = fldown = False
+        if flLeft:
+            player.rect.x -= 10
+        elif flRight:
+            player.rect.x += 10
+        if flup and not jump:
+            player.rect.y -= 10
+        elif fldown:
+            player.rect.y += 10
+        if jump:
+            player.rect.y -= jumpCount
+            if jumpCount > -jumpMax:
+                jumpCount -= 2
+            else:
+                jump = False
+
         screen.blit(level, (0, 0))
         all_sprites.draw(screen)
         all_sprites.update()
-        clock.tick(10)
+        clock.tick(15)
         pygame.display.flip()
 
 
 def load_level_2():
+    pygame.mixer.music.load('data/boss2.ogg')
+    pygame.mixer.music.play()
     level = pygame.transform.scale(load_image('level2.png'), size)
     screen.blit(level, (0, 0))
     player = Hero()
+    flLeft = flRight = flup = fldown = False
+    jump = False
+    jumpCount = 0
+    jumpMax = 15
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_DOWN:
-                    player.rect.y += 10
-                elif event.key == pygame.K_UP:
-                    player.rect.y -= 10
-                elif event.key == pygame.K_LEFT:
-                    player.rect.x -= 10
-                elif event.key == pygame.K_RIGHT:
-                    player.rect.x += 10
+                if event.key == pygame.K_s:
+                    fldown = True
+                elif not jump and event.key == pygame.K_SPACE:
+                    jump = True
+                    jumpCount = jumpMax
+                elif event.key == pygame.K_w:
+                    flup = True
+                elif event.key == pygame.K_a:
+                    flLeft = True
+                elif event.key == pygame.K_d:
+                    flRight = True
+            elif event.type == pygame.KEYUP:
+                if event.key in [pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s]:
+                    flLeft = flRight = flup = fldown = False
+        if flLeft:
+            player.rect.x -= 10
+        elif flRight:
+            player.rect.x += 10
+        if flup and not jump:
+            player.rect.y -= 10
+        elif fldown:
+                player.rect.y += 10
+        if jump:
+            player.rect.y -= jumpCount
+            if jumpCount > -jumpMax:
+                jumpCount -= 2
+            else:
+                jump = False
         screen.blit(level, (0, 0))
         all_sprites.draw(screen)
         all_sprites.update()
@@ -89,22 +140,48 @@ def load_level_2():
 
 
 def load_level_3():
+    pygame.mixer.music.load('data/boss3.mp3')
+    pygame.mixer.music.play()
     level = pygame.transform.scale(load_image('level3.png'), size)
     screen.blit(level, (0, 0))
     player = Hero()
+    flLeft = flRight = flup = fldown = False
+    jump = False
+    jumpCount = 0
+    jumpMax = 15
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_DOWN:
-                    player.rect.y += 10
-                elif event.key == pygame.K_UP:
-                    player.rect.y -= 10
-                elif event.key == pygame.K_LEFT:
-                    player.rect.x -= 10
-                elif event.key == pygame.K_RIGHT:
-                    player.rect.x += 10
+                if event.key == pygame.K_s:
+                    fldown = True
+                elif not jump and event.key == pygame.K_SPACE:
+                    jump = True
+                    jumpCount = jumpMax
+                elif event.key == pygame.K_w:
+                    flup = True
+                elif event.key == pygame.K_a:
+                    flLeft = True
+                elif event.key == pygame.K_d:
+                    flRight = True
+            elif event.type == pygame.KEYUP:
+                if event.key in [pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s]:
+                    flLeft = flRight = flup = fldown = False
+        if flLeft:
+            player.rect.x -= 10
+        elif flRight:
+            player.rect.x += 10
+        if flup and not jump:
+            player.rect.y -= 10
+        elif fldown:
+                player.rect.y += 10
+        if jump:
+            player.rect.y -= jumpCount
+            if jumpCount > -jumpMax:
+                jumpCount -= 2
+            else:
+                jump = False
         screen.blit(level, (0, 0))
         all_sprites.draw(screen)
         all_sprites.update()
@@ -117,6 +194,8 @@ def start_screen():
     fon = pygame.transform.scale(load_image('fon.jpg'), (size))
     screen.blit(fon, (0, 0))
     screen.blit(play, (350, 300))
+    pygame.mixer.music.load('data/title.ogg')
+    pygame.mixer.music.play()
 
     while True:
         for event in pygame.event.get():
@@ -146,10 +225,13 @@ def level_menu_screen():
             elif event.type == pygame.KEYDOWN or \
                     event.type == pygame.MOUSEBUTTONDOWN:
                 if 200 < pygame.mouse.get_pos()[0] < 350 and 250 < pygame.mouse.get_pos()[1] < 400:
+                    pygame.mixer.music.stop()
                     load_level_1()
                 elif 425 < pygame.mouse.get_pos()[0] < 575 and 250 < pygame.mouse.get_pos()[1] < 400:
+                    pygame.mixer.music.stop()
                     load_level_2()
                 elif 650 < pygame.mouse.get_pos()[0] < 800 and 250 < pygame.mouse.get_pos()[1] < 400:
+                    pygame.mixer.music.stop()
                     load_level_3()
         pygame.display.flip()
 
