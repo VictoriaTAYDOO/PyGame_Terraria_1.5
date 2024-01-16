d315e3747d40bb93a324bb694b330e00b5b0ebad
anastasiakrahmalnikova <anastasia.krakhmalnikova@yandex.ru>
2024-01-16 20:41:50 +0300

    shooting gun commit

--- main.py
+++ main.py
@@ -50,6 +50,25 @@ def terminate():
     sys.exit()
 
 
+def calculate_new_xy(old_xy, speed, angle_in_radians):
+    new_x = old_xy[0] + (speed * math.cos(angle_in_radians))
+    new_y = old_xy[1] + (speed * math.sin(angle_in_radians))
+    return new_x, new_y
+
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, image, coords, direction, speed):
+        super().__init__(all_sprites)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.center = (coords[0], coords[1])
+        self.direction = math.radians(direction)
+        self.speed = speed
+
+    def update(self):
+        self.rect.center = calculate_new_xy(self.rect.center, self.speed, self.direction)
+
+
 class Hero(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__(all_sprites)
@@ -98,7 +117,7 @@ def load_level_1():
     level = pygame.transform.scale(load_image('level1.png'), size)
     screen.blit(level, (0, 0))
     player = Hero()
-    flLeft = flRight = flup = False
+    flLeft = flRight = flup = shoot = False
 
     while True:
         for event in pygame.event.get():
@@ -122,6 +141,9 @@ def load_level_1():
                     flLeft = flRight = False
                 if event.key == pygame.K_SPACE:
                     flup = False
+            if event.type == pygame.MOUSEBUTTONUP:
+                if player.choice != 0:
+                    shoot = True
         if flLeft:
             player.rect.x -= 10
         elif flRight:
@@ -156,6 +178,7 @@ def load_level_1():
         gun_l = pygame.transform.scale(load_image('the_undertaker_l.png'), (46, 24))
         mgun = pygame.transform.scale(load_image('megashark.png'), (70, 28))
         mgun_l = pygame.transform.scale(load_image('megashark_l.png'), (70, 28))
+        bullet_im = pygame.transform.scale(load_image('bullet.png'), (20, 2))
 
         screen.blit(frame, (30, 20))
 
@@ -173,8 +196,17 @@ def load_level_1():
                         angle = -int((180 / math.pi) * -math.atan2(rel_y, rel_x))
                         rotated_image, rect = rotate(gun_l, angle + 180, pivot, offset)
                         screen.blit(rotated_image, rect)
+                        if shoot:
+                            offset = pygame.math.Vector2(15, 5)
+                            rotated_image, a = rotate(bullet_im, angle, pivot, offset)
+                            bullet = Bullet(rotated_image, (
+                            int(pivot[0] + offset.rotate(angle)[0] * 3.7), int(pivot[1] + offset.rotate(angle)[1] * 3.7)),
+                                            angle, 20)
+                            shoot = False
                     else:
                         screen.blit(gun_l, (player.rect.x - 35, player.rect.y + 20))
+                        if shoot:
+                            shoot = False
                 else:
                     pivot = [player.rect.x + 105, player.rect.y + 40]
                     offset = pygame.math.Vector2(15, -10)
@@ -184,8 +216,16 @@ def load_level_1():
                         angle = -int((180 / math.pi) * -math.atan2(rel_y, rel_x))
                         rotated_image, rect = rotate(gun, angle, pivot, offset)
                         screen.blit(rotated_image, rect)
+                        if shoot:
+                            offset = pygame.math.Vector2(15, -5)
+                            rotated_image, a = rotate(bullet_im, angle, pivot, offset)
+                            bullet = Bullet(rotated_image, (int(pivot[0] + offset.rotate(angle)[0] * 3.7),
+                                                            int(pivot[1] + offset.rotate(angle)[1] * 3.7)), angle, 20)
+                            shoot = False
                     else:
                         screen.blit(gun, (player.rect.x + 100, player.rect.y + 20))
+                        if shoot:
+                            shoot = False
             else:
                 if player.left:
                     pivot = [player.rect.x + 40, player.rect.y + 45]
@@ -196,8 +236,17 @@ def load_level_1():
                         angle = -int((180 / math.pi) * -math.atan2(rel_y, rel_x))
                         rotated_image, rect = rotate(gun_l, angle + 180, pivot, offset)
                         screen.blit(rotated_image, rect)
+                        if shoot:
+                            offset = pygame.math.Vector2(15, 5)
+                            rotated_image, a = rotate(bullet_im, angle, pivot, offset)
+                            bullet = Bullet(rotated_image, (
+                            int(pivot[0] + offset.rotate(angle)[0] * 3.7), int(pivot[1] + offset.rotate(angle)[1] * 3.7)),
+                                            angle, 20)
+                            shoot = False
                     else:
                         screen.blit(gun_l, (player.rect.x - 5, player.rect.y + 25))
+                        if shoot:
+                            shoot = False
                 else:
                     pivot = [player.rect.x + 75, player.rect.y + 45]
                     offset = pygame.math.Vector2(15, -10)
@@ -207,8 +256,16 @@ def load_level_1():
                         angle = -int((180 / math.pi) * -math.atan2(rel_y, rel_x))
                         rotated_image, rect = rotate(gun, angle, pivot, offset)
                         screen.blit(rotated_image, rect)
+                        if shoot:
+                            offset = pygame.math.Vector2(15, -5)
+                            rotated_image, a = rotate(bullet_im, angle, pivot, offset)
+                            bullet = Bullet(rotated_image, (int(pivot[0] + offset.rotate(angle)[0] * 3.7),
+                                                            int(pivot[1] + offset.rotate(angle)[1] * 3.7)), angle, 20)
+                            shoot = False
                     else:
                         screen.blit(gun, (player.rect.x + 65, player.rect.y + 25))
+                        if shoot:
+                            shoot = False
 
         if player.choice == 2:
             if player.is_flying:
